62d3437c4f06d7b008b778147e4648699fd330a6
Iury França <iurygfranca@gmail.com>
2025-07-01 20:38:32 -0400

    feat: enhance ButtonNavbar component with props for dynamic rendering 🔧

--- src/components/ButtonNavbar.tsx
+++ src/components/ButtonNavbar.tsx
@@ -1,3 +1,14 @@
-export default function ButtonNavbar() {
-  return <div>AA</div>;
+import { Button } from '@/components/ui/button';
+
+interface ButtonNavbarProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+export default function ButtonNavbar({ href, children }: ButtonNavbarProps) {
+  return (
+    <a href={href}>
+      <Button className="">{children}</Button>
+    </a>
+  );
 }
